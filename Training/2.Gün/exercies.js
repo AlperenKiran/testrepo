28255675f9adfde4ed5fb1c94f3541371c72cb8f
DESKTOP-F6PNV4H\Alperen <alperenkiran11@gmail.com>
2023-02-01 16:35:26 +0300

    training 2

---
+++ Training/2.Gün/exercies.js
@@ -0,0 +1,23 @@
+let challenge = "30 days of javascipt"
+console.log(challenge)
+console.log("String")
+console.log("browser".length)
+console.log("alperen".toUpperCase())
+console.log("YUSUF".toLowerCase())
+// console.log("alperen kiran"("kiran".sub))
+// let sprint = "tayfun"+" "+ "elben"
+// console.log(sprint.substring("elben"," "))
+console.log("alperen kiran script".includes("script"))
+console.log("alperen".split(""))
+console.log("30 days of javascript".split(" "))
+console.log("30 days of javascript".charAt(11))
+console.log("30 days of javascript".match(/a/gi))
+console.log(typeof '10')
+
+let alp = 10
+let alper= "10"
+
+console.log(alp==alper)
+
+let on = "python"
+console.log(on.includes("on"))
