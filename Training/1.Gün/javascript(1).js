c3d59cb6e0849dd4c641c956c1ed26560eed7e43
DESKTOP-F6PNV4H\Alperen <alperenkiran11@gmail.com>
2023-02-01 16:24:03 +0300

    first commit

---
+++ Training/1.Gün/javascript(1).js
@@ -0,0 +1,18 @@
+//console.log ("Bu benim ilk external scriptim")
+
+console.log(typeof "alperen")
+console.log(typeof 5)
+console.log(typeof true)
+console.log(typeof null )
+console.log(typeof undefined) 
+
+let firstName = "Alperen"
+let softwareDay = 60
+let trueFalse = true
+let business = null
+let phoneNumber = undefined
+
+const PI = 3.14
+const gravity=9.81
+
+console.log (firstName,softwareDay,trueFalse,business,phoneNumber,PI,gravity)
